2e3963252c99c1ce46f38ab44faa16f1b334209e
Pentalimbed <pentalimbed@gmail.com>
2024-08-06 14:36:22 +0100

    Squashed commit of the following:
    commit 14c6884a9188174606577dc81f621aecb6e4a7cc
    Author: Pentalimbed <partenairejunior@sina.com>
    Date:   Sun Feb 4 21:48:56 2024 +0000
    
        feat: add physical sky

---
+++ src/Features/PhysicalSky.h
@@ -0,0 +1,359 @@
+#pragma once
+
+#include "Buffer.h"
+#include "Feature.h"
+
+namespace RE
+{
+	class BSSkyShaderProperty : public BSShaderProperty
+	{
+	public:
+		inline static constexpr auto RTTI = RTTI_BSSkyShaderProperty;
+		inline static constexpr auto Ni_RTTI = NiRTTI_BSSkyShaderProperty;
+
+		enum class SkyObjectType
+		{
+			Sun = 0x0,
+			SunGlare = 0x1,
+			Atmosphere = 0x2,
+			Clouds = 0x3,
+			SkyQuad = 0x4,
+			Stars = 0x5,
+			Moon = 0x6,
+			MoonShadow = 0x7,
+		};
+
+		// members
+		NiColorA color;                                        // 88
+		NiPointer<NiSourceTexture> baseTexture;                // 98
+		NiPointer<NiSourceTexture> blendTexture;               // A0
+		NiPointer<NiSourceTexture> noiseGradTexture;           // A8
+		BSFixedString baseTexturePath;                         // B0
+		float blendValue;                                      // B8
+		uint16_t cloudLayer;                                   // BC
+		bool fadeSecondTexture;                                // BE
+		stl::enumeration<SkyObjectType, uint32_t> objectType;  // C0
+	};
+	static_assert(sizeof(BSSkyShaderProperty) == 0xC8);
+}  // namespace RE
+
+//////////////////////////////////////////////////////////////////////////
+
+struct Orbit
+{
+	// in rad
+	float azimuth = 0;  // rad, 0 being E-W
+	float zenith = 0;   // rad, 0 being up
+	// in [-1, 1]
+	float drift = 7.f * RE::NI_PI / 180.f;  // rad, moving to the sides
+
+	RE::NiPoint3 getDir(float t);  // t = fraction of a cycle, start at the bottom
+	RE::NiPoint3 getTangent(float t);
+};
+
+struct Trajectory
+{
+	Orbit minima, maxima;
+	// in days
+	float period_orbital = 1;  // circling one orbit
+	float offset_orbital = 0;  // add to gameDaysPassed
+	float period_long = 364;   // from minima to maxima and back
+	float offset_long = -10;   // start from the mean of minima and maxima
+
+	Orbit getMixedOrbit(float gameDaysPassed);
+	RE::NiPoint3 getDir(float gameDaysPassed);
+	RE::NiPoint3 getTangent(float gameDaysPassed);
+};
+
+struct PhysicalSky : public Feature
+{
+	constexpr static uint16_t s_transmittance_width = 256;
+	constexpr static uint16_t s_transmittance_height = 64;
+	constexpr static uint16_t s_multiscatter_width = 32;
+	constexpr static uint16_t s_multiscatter_height = 32;
+	constexpr static uint16_t s_sky_view_width = 200;
+	constexpr static uint16_t s_sky_view_height = 150;
+	constexpr static uint16_t s_aerial_perspective_width = 32;
+	constexpr static uint16_t s_aerial_perspective_height = 32;
+	constexpr static uint16_t s_aerial_perspective_depth = 32;
+
+	static PhysicalSky* GetSingleton()
+	{
+		static PhysicalSky singleton;
+		return std::addressof(singleton);
+	}
+
+	virtual inline std::string GetName() override { return "Physical Sky"; }
+	virtual inline std::string GetShortName() override { return "PhysicalSky"; }
+	virtual inline std::string_view GetShaderDefineName() override { return "PHYS_SKY"; }
+	virtual bool HasShaderDefine(RE::BSShader::Type) override;
+
+	uint32_t current_sky_obj_type = 0;  // 1-sun, 2-masser, 3-secunda
+	uint32_t current_moon_phases[2];
+
+	struct Settings
+	{
+		// GENRERAL
+		bool enable_sky = false;
+		bool enable_aerial = true;
+
+		// PERFORMANCE
+		uint transmittance_step = 40;
+		uint multiscatter_step = 20;
+		uint multiscatter_sqrt_samples = 4;
+		uint skyview_step = 30;
+		float aerial_perspective_max_dist = 80;  // in km
+
+		// WORLD
+		float unit_scale = 5;
+		float bottom_z = -15000;        // in game unit
+		float planet_radius = 6.36e3f;  // 6360 km
+		float atmos_thickness = 100.f;  // 20 km
+		float3 ground_albedo = { .2f, .2f, .2f };
+
+		// LIGHTING
+		float3 sunlight_color = float3{ 1.0f, 0.949f, 0.937f } * 15.f;
+
+		float3 moonlight_color = float3{ .9f, .9f, 1.f } * .1f;
+		bool phase_dep_moonlight = false;
+		float masser_moonlight_min = 0.1f;
+		float3 masser_moonlight_color = float3{ .1f, .07f, .07f };
+		float secunda_moonlight_min = 0.1f;
+		float3 secunda_moonlight_color = float3{ .9f, .9f, 1.f } * .2f;
+
+		float2 light_transition_angles = float2{ -10.f, -14.f } * RE::NI_PI / 180.0;
+
+		bool enable_vanilla_clouds = true;
+		float cloud_height = 4.f;  // km
+		float cloud_saturation = .7f;
+		float cloud_mult = 1.f;
+		float cloud_atmos_scatter = 3.f;
+
+		float cloud_phase_g0 = .42f;
+		float cloud_phase_g1 = -.3f;
+		float cloud_phase_w = .3f;
+		float cloud_alpha_heuristics = 0.3f;
+		float cloud_color_heuristics = 0.1f;
+
+		bool override_dirlight_color = false;
+		bool override_dirlight_dir = false;
+		bool moonlight_follows_secunda = true;
+		float dirlight_transmittance_mix = 0;
+		float treelod_saturation = .1f;
+		float treelod_mult = .25f;
+
+		// CELESTIALS
+		bool override_vanilla_celestials = true;
+		// - Sun
+		float3 sun_disc_color = float3{ 1.f, 0.949f, 0.937f } * 50.f;
+		float sun_angular_radius = .5f * RE::NI_PI / 180.0f;  // in rad
+
+		bool override_sun_traj = false;
+		Trajectory sun_trajectory{
+			.minima = { .zenith = -40 * RE::NI_PI / 180.0f, .drift = -23.5f * RE::NI_PI / 180.0f },
+			.maxima = { .zenith = -40 * RE::NI_PI / 180.0f, .drift = 23.5f * RE::NI_PI / 180.0f }
+		};
+
+		// - Moons
+		bool override_masser_traj = false;
+		Trajectory masser_trajectory = {
+			.minima = { .zenith = 0, .drift = -.174f },
+			.maxima = { .zenith = 0, .drift = -.174f },
+			.period_orbital = .25f / 0.23958333333f,  // po3 values :)
+			.offset_orbital = 0.7472f
+		};
+		float masser_angular_radius = 10.f * RE::NI_PI / 180.0f;
+		float masser_brightness = .7f;
+
+		bool override_secunda_traj = false;
+		Trajectory secunda_trajectory = {
+			.minima = { .zenith = 0, .drift = -.423f },
+			.maxima = { .zenith = 0, .drift = -.423f },
+			.period_orbital = .25f / 0.2375f,
+			.offset_orbital = 0.3775f
+		};
+		float secunda_angular_radius = 4.5f * RE::NI_PI / 180.0f;
+		float secunda_brightness = .7f;
+
+		float stars_brightness = 1;
+
+		// ATMOSPHERE
+		float ap_inscatter_mix = 1.f;
+		float ap_transmittance_mix = 1.f;
+
+		float3 rayleigh_scatter = { 6.6049f, 12.345f, 29.413f };  // in megameter^-1
+		float3 rayleigh_absorption = { 0.f, 0.f, 0.f };
+		float rayleigh_decay = 1 / 8.69645f;  // in km^-1
+
+		float aerosol_phase_func_g = 0.8f;
+		float3 aerosol_scatter = { 3.996f, 3.996f, 3.996f };
+		float3 aerosol_absorption = { .444f, .444f, .444f };
+		float aerosol_decay = 1 / 1.2f;
+
+		float3 ozone_absorption = { 2.2911f, 1.5404f, 0 };
+		float ozone_altitude = 22.3499f + 35.66071f * .5f;  // in km
+		float ozone_thickness = 35.66071f;
+
+	} settings;
+
+	struct PhysSkySB
+	{
+		uint enable_sky;
+		uint enable_aerial;
+
+		// PERFORMANCE
+		uint transmittance_step;
+		uint multiscatter_step;
+		uint multiscatter_sqrt_samples;
+		uint skyview_step;
+		float aerial_perspective_max_dist;
+
+		// WORLD
+		float unit_scale;
+		float bottom_z;
+		float planet_radius;
+		float atmos_thickness;
+		float3 ground_albedo;
+
+		// LIGHTING
+		uint override_dirlight_color;
+		float dirlight_transmittance_mix;
+		float treelod_saturation;
+		float treelod_mult;
+
+		uint enable_vanilla_clouds;
+		float cloud_height;
+		float cloud_saturation;
+		float cloud_mult;
+		float cloud_atmos_scatter;
+
+		float cloud_phase_g0;
+		float cloud_phase_g1;
+		float cloud_phase_w;
+		float cloud_alpha_heuristics;
+		float cloud_color_heuristics;
+
+		// CELESTIAL
+		uint override_vanilla_celestials;
+		float3 sun_disc_color;
+		float sun_aperture_cos;
+		float sun_aperture_rcp_sin;
+
+		float masser_aperture_cos;
+		float masser_brightness;
+
+		float secunda_aperture_cos;
+		float secunda_brightness;
+
+		// ATMOSPHERE
+		float ap_inscatter_mix;
+		float ap_transmittance_mix;
+
+		float3 rayleigh_scatter;
+		float3 rayleigh_absorption;
+		float rayleigh_decay;
+
+		float aerosol_phase_func_g;
+		float3 aerosol_scatter;
+		float3 aerosol_absorption;
+		float aerosol_decay;
+
+		float3 ozone_absorption;
+		float ozone_altitude;
+		float ozone_thickness;
+
+		// DYNAMIC
+		float3 dirlight_dir;
+		float3 dirlight_color;
+		float3 sun_dir;
+		float3 masser_dir;
+		float3 masser_upvec;
+		float3 secunda_dir;
+		float3 secunda_upvec;
+
+		float horizon_penumbra;
+
+		float cam_height_km;  // in km
+
+	} phys_sky_sb_data;
+	std::unique_ptr<StructuredBuffer> phys_sky_sb = nullptr;
+
+	struct SkyPerGeometrySB
+	{
+		uint sky_object_type;
+	};
+	std::unique_ptr<StructuredBuffer> sky_per_geo_sb = nullptr;
+
+	std::unique_ptr<Texture2D> transmittance_lut = nullptr;
+	std::unique_ptr<Texture2D> multiscatter_lut = nullptr;
+	std::unique_ptr<Texture2D> sky_view_lut = nullptr;
+	std::unique_ptr<Texture3D> aerial_perspective_lut = nullptr;
+
+	winrt::com_ptr<ID3D11ComputeShader> transmittance_program = nullptr;
+	winrt::com_ptr<ID3D11ComputeShader> multiscatter_program = nullptr;
+	winrt::com_ptr<ID3D11ComputeShader> sky_view_program = nullptr;
+	winrt::com_ptr<ID3D11ComputeShader> aerial_perspective_program = nullptr;
+
+	winrt::com_ptr<ID3D11SamplerState> transmittance_sampler = nullptr;
+	winrt::com_ptr<ID3D11SamplerState> sky_view_sampler = nullptr;
+
+	virtual void Load(json& o_json) override;
+	virtual void Save(json& o_json) override;
+
+	virtual void SetupResources() override;
+	void CompileComputeShaders();
+
+	inline bool CheckComputeShaders()
+	{
+		bool result = transmittance_program && multiscatter_program && sky_view_program;
+		if (settings.enable_aerial)
+			result = result && aerial_perspective_program;
+		return result;
+	}
+	bool NeedLutsUpdate();
+
+	virtual void Reset() override;
+	void UpdateBuffer();
+	void UpdateOrbitsAndHeight();
+
+	virtual void DrawSettings() override;
+	void SettingsGeneral();
+	void SettingsWorld();
+	void SettingsLighting();
+	void SettingsClouds();
+	void SettingsCelestials();
+	void SettingsAtmosphere();
+	void SettingsDebug();
+
+	virtual void Draw(const RE::BSShader* shader, const uint descriptor) override;
+	void GenerateLuts();
+	void ModifyLighting();
+	void ModifySky(const RE::BSShader* shader, const uint descriptor);
+
+	virtual inline void RestoreDefaultSettings() override { settings = {}; };
+	virtual void ClearShaderCache() override;
+
+	virtual inline void PostPostLoad() override { Hooks::Install(); }
+
+	struct Hooks
+	{
+		struct BSSkyShader_SetupGeometry
+		{
+			static void thunk(RE::BSShader* a_this, RE::BSRenderPass* a_pass, uint32_t a_renderFlags);
+			static inline REL::Relocation<decltype(thunk)> func;
+		};
+
+		struct NiPoint3_Normalize
+		{
+			// at Sun::Update
+			static void thunk(RE::NiPoint3* a_this);
+			static inline REL::Relocation<decltype(thunk)> func;
+		};
+
+		static void Install()
+		{
+			stl::write_thunk_call<NiPoint3_Normalize>(REL::RelocationID(25798, 26352).address() + REL::Relocate(0x6A8, 0x753));
+			stl::write_vfunc<0x6, BSSkyShader_SetupGeometry>(RE::VTABLE_BSSkyShader[0]);
+		}
+	};
+};
---
+++ src/Features/PhysicalSky/PSSystem.cpp
@@ -0,0 +1,340 @@
+#include "../PhysicalSky.h"
+
+#include "PSCommon.h"
+
+#include "Util.h"
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(Orbit, azimuth, zenith, drift)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(Trajectory,
+	minima, maxima,
+	period_orbital, offset_orbital,
+	period_long, offset_long)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(PhysicalSky::Settings,
+	enable_sky,
+	enable_aerial,
+	transmittance_step,
+	multiscatter_step,
+	multiscatter_sqrt_samples,
+	skyview_step,
+	aerial_perspective_max_dist,
+	unit_scale,
+	bottom_z,
+	planet_radius,
+	atmos_thickness,
+	ground_albedo,
+	enable_vanilla_clouds,
+	cloud_height,
+	cloud_saturation,
+	cloud_mult,
+	cloud_phase_g0,
+	cloud_phase_g1,
+	cloud_phase_w,
+	cloud_alpha_heuristics,
+	cloud_color_heuristics,
+	override_dirlight_color,
+	override_dirlight_dir,
+	moonlight_follows_secunda,
+	dirlight_transmittance_mix,
+	treelod_saturation,
+	treelod_mult,
+	sunlight_color,
+	moonlight_color,
+	phase_dep_moonlight,
+	masser_moonlight_min,
+	masser_moonlight_color,
+	secunda_moonlight_min,
+	secunda_moonlight_color,
+	light_transition_angles,
+	override_vanilla_celestials,
+	sun_disc_color,
+	sun_angular_radius,
+	override_sun_traj,
+	sun_trajectory,
+	override_masser_traj,
+	masser_trajectory,
+	masser_angular_radius,
+	masser_brightness,
+	override_secunda_traj,
+	secunda_trajectory,
+	secunda_angular_radius,
+	secunda_brightness,
+	stars_brightness,
+	ap_inscatter_mix,
+	ap_transmittance_mix,
+	rayleigh_scatter,
+	rayleigh_absorption,
+	rayleigh_decay,
+	aerosol_phase_func_g,
+	aerosol_scatter,
+	aerosol_absorption,
+	aerosol_decay,
+	ozone_absorption,
+	ozone_altitude,
+	ozone_thickness)
+
+RE::NiPoint3 Orbit::getDir(float t)
+{
+	float t_rad = t * 2 * RE::NI_PI;
+
+	RE::NiPoint3 result = { sin(t_rad) * cos(drift), cos(t_rad) * cos(drift), sin(drift) };
+
+	RE::NiMatrix3 rotmat = RE::NiMatrix3(0, 0, azimuth) * RE::NiMatrix3(zenith + .5f * RE::NI_PI, 0, 0);
+	result = rotmat * result;
+
+	result.Unitize();
+
+	return result;
+}
+
+RE::NiPoint3 Orbit::getTangent(float t)
+{
+	float t_rad = t * 2 * RE::NI_PI;
+
+	RE::NiPoint3 result = { cos(t_rad), 0, sin(t_rad) };
+
+	RE::NiMatrix3 rotmat;
+	rotmat.SetEulerAnglesXYZ(zenith, 0, azimuth);
+	result = rotmat * result;
+
+	return result;
+}
+
+Orbit Trajectory::getMixedOrbit(float gameDaysPassed)
+{
+	auto lerp = sin((gameDaysPassed + offset_long) / period_long * 2 * RE::NI_PI) * .5f + .5f;
+	Orbit orbit = {
+		.azimuth = std::lerp(minima.azimuth, maxima.azimuth, lerp),
+		.zenith = std::lerp(minima.zenith, maxima.zenith, lerp),
+		.drift = std::lerp(minima.drift, maxima.drift, lerp)
+	};
+	return orbit;
+}
+
+RE::NiPoint3 Trajectory::getDir(float gameDaysPassed)
+{
+	auto t = (gameDaysPassed + offset_orbital) / period_orbital;
+	return getMixedOrbit(gameDaysPassed).getDir(t);
+}
+
+RE::NiPoint3 Trajectory::getTangent(float gameDaysPassed)
+{
+	auto t = (gameDaysPassed + offset_orbital) / period_orbital;
+	return getMixedOrbit(gameDaysPassed).getTangent(t);
+}
+
+void PhysicalSky::Load(json& o_json)
+{
+	if (o_json[GetName()].is_object())
+		settings = o_json[GetName()];
+
+	Feature::Load(o_json);
+}
+
+void PhysicalSky::Save(json& o_json)
+{
+	o_json[GetName()] = settings;
+}
+
+void PhysicalSky::UpdateBuffer()
+{
+	float sun_aperture_cos = cos(settings.sun_angular_radius);
+	float sun_aperture_rcp_sin = 1.f / sqrt(1 - sun_aperture_cos * sun_aperture_cos);  // I trust u compiler
+
+	phys_sky_sb_data = {
+		.enable_sky = settings.enable_sky && NeedLutsUpdate(),
+		.enable_aerial = settings.enable_aerial,
+		.transmittance_step = settings.transmittance_step,
+		.multiscatter_step = settings.multiscatter_step,
+		.multiscatter_sqrt_samples = settings.multiscatter_sqrt_samples,
+		.skyview_step = settings.skyview_step,
+		.aerial_perspective_max_dist = settings.aerial_perspective_max_dist,
+		.unit_scale = settings.unit_scale,
+		.bottom_z = settings.bottom_z,
+		.planet_radius = settings.planet_radius,
+		.atmos_thickness = settings.atmos_thickness,
+		.ground_albedo = settings.ground_albedo,
+		.override_dirlight_color = settings.override_dirlight_color,
+		.dirlight_transmittance_mix = settings.override_dirlight_color ? 1.f : settings.dirlight_transmittance_mix,
+		.treelod_saturation = settings.treelod_saturation,
+		.treelod_mult = settings.treelod_mult,
+		.enable_vanilla_clouds = settings.enable_vanilla_clouds,
+		.cloud_height = settings.cloud_height,
+		.cloud_saturation = settings.cloud_saturation,
+		.cloud_mult = settings.cloud_mult,
+		.cloud_atmos_scatter = settings.cloud_atmos_scatter,
+		.cloud_phase_g0 = settings.cloud_phase_g0,
+		.cloud_phase_g1 = settings.cloud_phase_g1,
+		.cloud_phase_w = settings.cloud_phase_w,
+		.cloud_alpha_heuristics = settings.cloud_alpha_heuristics,
+		.cloud_color_heuristics = settings.cloud_color_heuristics,
+		.override_vanilla_celestials = settings.override_vanilla_celestials,
+		.sun_disc_color = settings.sun_disc_color,
+		.sun_aperture_cos = sun_aperture_cos,
+		.sun_aperture_rcp_sin = sun_aperture_rcp_sin,
+		.masser_aperture_cos = cos(settings.masser_angular_radius),
+		.masser_brightness = settings.masser_brightness,
+		.secunda_aperture_cos = cos(settings.secunda_angular_radius),
+		.secunda_brightness = settings.secunda_brightness,
+		.ap_inscatter_mix = settings.ap_inscatter_mix,
+		.ap_transmittance_mix = settings.ap_transmittance_mix,
+		.rayleigh_scatter = settings.rayleigh_scatter * 1e-3f,  // km^-1
+		.rayleigh_absorption = settings.rayleigh_absorption * 1e-3f,
+		.rayleigh_decay = settings.rayleigh_decay,
+		.aerosol_phase_func_g = settings.aerosol_phase_func_g,
+		.aerosol_scatter = settings.aerosol_scatter * 1e-3f,
+		.aerosol_absorption = settings.aerosol_absorption * 1e-3f,
+		.aerosol_decay = settings.aerosol_decay,
+		.ozone_absorption = settings.ozone_absorption * 1e-3f,
+		.ozone_altitude = settings.ozone_altitude,
+		.ozone_thickness = settings.ozone_thickness
+	};
+
+	// DYNAMIC STUFF
+	if (phys_sky_sb_data.enable_sky)
+		UpdateOrbitsAndHeight();
+
+	phys_sky_sb->Update(&phys_sky_sb_data, sizeof(phys_sky_sb_data));
+}
+
+void PhysicalSky::UpdateOrbitsAndHeight()
+{
+	// height part
+	RE::NiPoint3 cam_pos = { 0, 0, 0 };
+	if (auto cam = RE::PlayerCamera::GetSingleton(); cam && cam->cameraRoot) {
+		cam_pos = cam->cameraRoot->world.translate;
+		phys_sky_sb_data.cam_height_km = (cam_pos.z - settings.bottom_z) * settings.unit_scale * 1.428e-5f + settings.planet_radius;
+	}
+
+	// orbits
+	auto sky = RE::Sky::GetSingleton();
+	// auto sun = sky->sun;
+	auto masser = sky->masser;
+	auto secunda = sky->secunda;
+	// auto stars = sky->stars;
+
+	RE::NiPoint3 sun_dir;
+	auto calendar = RE::Calendar::GetSingleton();
+	if (calendar) {
+		float game_days = getDayInYear();  // Last Seed = 8
+		if (settings.override_sun_traj) {
+			sun_dir = settings.sun_trajectory.getDir(game_days);
+		} else {
+			sun_dir = Orbit{}.getDir(getVanillaSunLerpFactor());
+		}
+
+		if (settings.override_masser_traj) {
+			auto masser_dir = settings.masser_trajectory.getDir(game_days);
+			auto masser_up = masser_dir.Cross(settings.masser_trajectory.getTangent(game_days));
+
+			phys_sky_sb_data.masser_dir = { masser_dir.x, masser_dir.y, masser_dir.z };
+			phys_sky_sb_data.masser_upvec = { masser_up.x, masser_up.y, masser_up.z };
+		} else if (masser) {
+			auto masser_dir = masser->moonMesh->world.translate - cam_pos;
+			masser_dir.Unitize();
+			auto masser_upvec = masser->moonMesh->world.rotate * RE::NiPoint3{ 0, 1, 0 };
+
+			phys_sky_sb_data.masser_dir = { masser_dir.x, masser_dir.y, masser_dir.z };
+			phys_sky_sb_data.masser_upvec = { masser_upvec.x, masser_upvec.y, masser_upvec.z };
+		}
+
+		if (settings.override_secunda_traj) {
+			auto secunda_dir = settings.secunda_trajectory.getDir(game_days);
+			auto secunda_up = secunda_dir.Cross(settings.secunda_trajectory.getTangent(game_days));
+
+			phys_sky_sb_data.secunda_dir = { secunda_dir.x, secunda_dir.y, secunda_dir.z };
+			phys_sky_sb_data.secunda_upvec = { secunda_up.x, secunda_up.y, secunda_up.z };
+		} else if (secunda) {
+			auto secunda_dir = secunda->moonMesh->world.translate - cam_pos;
+			secunda_dir.Unitize();
+			auto secunda_upvec = secunda->moonMesh->world.rotate * RE::NiPoint3{ 0, 1, 0 };
+
+			phys_sky_sb_data.secunda_dir = { secunda_dir.x, secunda_dir.y, secunda_dir.z };
+			phys_sky_sb_data.secunda_upvec = { secunda_upvec.x, secunda_upvec.y, secunda_upvec.z };
+		}
+	}
+	phys_sky_sb_data.sun_dir = { sun_dir.x, sun_dir.y, sun_dir.z };
+
+	// sun or moon
+	float sun_dir_angle = asin(sun_dir.z);
+	float sun_moon_transition = (sun_dir_angle - settings.light_transition_angles.x) / (settings.light_transition_angles.y - settings.light_transition_angles.x);
+	if (sun_moon_transition < .5) {
+		phys_sky_sb_data.dirlight_color = (1.f - std::clamp(sun_moon_transition * 2.f, 0.f, 1.f)) * settings.sunlight_color;
+		phys_sky_sb_data.dirlight_dir = phys_sky_sb_data.sun_dir;
+	} else {  // moon
+		float3 moonlight = settings.moonlight_color;
+		if (settings.phase_dep_moonlight) {
+			float masser_phase = current_moon_phases[0] > 4 ? (current_moon_phases[0] - 4) * .25f : current_moon_phases[0] * .25f;
+			float3 masser_light = (1 - masser_phase * (1 - settings.masser_moonlight_min)) * settings.masser_moonlight_color;
+			float secunda_phase = current_moon_phases[1] > 4 ? (current_moon_phases[1] - 4) * .25f : current_moon_phases[1] * .25f;
+			float3 secunda_light = (1 - secunda_phase * (1 - settings.secunda_moonlight_min)) * settings.secunda_moonlight_color;
+			moonlight = masser_light + secunda_light;
+		}
+		phys_sky_sb_data.dirlight_color = std::clamp(sun_moon_transition * 2.f - 1, 0.f, 1.f) * moonlight;
+
+		phys_sky_sb_data.dirlight_dir = settings.moonlight_follows_secunda ? phys_sky_sb_data.secunda_dir : phys_sky_sb_data.masser_dir;
+	}
+
+	float sin_sun_horizon = phys_sky_sb_data.dirlight_dir.z;
+	float norm_dist = sin_sun_horizon * phys_sky_sb_data.sun_aperture_rcp_sin;
+	if (abs(norm_dist) < 1)
+		phys_sky_sb_data.horizon_penumbra = .5f + norm_dist * .5f;  // horizon penumbra, not accurate but good enough
+	else
+		phys_sky_sb_data.horizon_penumbra = sin_sun_horizon > 0;
+}
+
+void PhysicalSky::Hooks::NiPoint3_Normalize::thunk(RE::NiPoint3* a_this)
+{
+	auto feat = GetSingleton();
+	if (feat->phys_sky_sb_data.enable_sky && feat->settings.override_dirlight_dir) {
+		float3 mod_dirlight_dir = -feat->phys_sky_sb_data.dirlight_dir;
+		*a_this = { mod_dirlight_dir.x, mod_dirlight_dir.y, mod_dirlight_dir.z };
+	} else
+		func(a_this);
+}
+
+void PhysicalSky::Hooks::BSSkyShader_SetupGeometry::thunk(RE::BSShader* a_this, RE::BSRenderPass* a_pass, uint32_t a_renderFlags)
+{
+	func(a_this, a_pass, a_renderFlags);
+
+	auto feat = GetSingleton();
+
+	// get obj
+	feat->current_sky_obj_type = 0;
+
+	auto sky = RE::Sky::GetSingleton();
+	auto sun = sky->sun;
+	auto masser = sky->masser;
+	auto secunda = sky->secunda;
+	if (sun && (size_t)sun->sunBase.get() == (size_t)a_pass->geometry)
+		feat->current_sky_obj_type = 1;
+	else if (masser && (size_t)masser->moonMesh.get() == (size_t)a_pass->geometry)
+		feat->current_sky_obj_type = 2;
+	else if (secunda && (size_t)secunda->moonMesh.get() == (size_t)a_pass->geometry)
+		feat->current_sky_obj_type = 3;
+
+	if (feat->current_sky_obj_type == 2 || feat->current_sky_obj_type == 3)
+		if (auto shaderProperty = netimmerse_cast<RE::BSSkyShaderProperty*>(a_pass->shaderProperty))
+			if (auto tex = shaderProperty->baseTexture) {
+				if (feat->current_sky_obj_type == 2) {
+					for (uint i = 0; i < RE::Moon::Phase::kTotal; i++) {
+						RE::NiTexturePtr phase_tex = nullptr;
+						RE::BSShaderManager::GetTexture(masser->stateTextures[i].c_str(), true, phase_tex, false);
+						if (phase_tex == tex) {
+							feat->current_moon_phases[0] = i;
+							break;
+						}
+					}
+				} else {
+					for (uint i = 0; i < RE::Moon::Phase::kTotal; i++) {
+						RE::NiTexturePtr phase_tex = nullptr;
+						RE::BSShaderManager::GetTexture(secunda->stateTextures[i].c_str(), true, phase_tex, false);
+						if (phase_tex == tex) {
+							feat->current_moon_phases[1] = i;
+							break;
+						}
+					}
+				}
+			}
+
+	// GetSingleton()->current_sky_obj_type = skyrim_cast<RE::BSSkyShaderProperty*>(a_pass->shaderProperty)->objectType.underlying();
+}
